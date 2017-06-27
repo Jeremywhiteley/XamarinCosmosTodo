8c8bff991110c91811bc41dc7d58f87c626aff41
Jeremy Whiteley <jeremy@medtexter.com>
2017-06-27 14:00:52 -0700

    Initial check-in of module TodoDocumentDB

---
+++ TodoDocumentDB/Constants.cs
@@ -0,0 +1,13 @@
+﻿namespace TodoDocumentDB
+{
+	public static class Constants
+	{
+		// Azure DocumentDB
+		public static readonly string EndpointUri = "https://jertest.documents.azure.com:443/";
+		public static readonly string DatabaseName = "TodoList";
+		public static readonly string CollectionName = "UserItems";
+
+		public static string ResourceTokenBrokerUrl = "https://jertestdb.azurewebsites.net";
+	}
+}
+
